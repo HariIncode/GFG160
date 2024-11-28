2b38513000e8ccb796912b6855510c08be37c2c2
Hari haran <hariharan15060@gmail.com>
2024-11-28 20:22:44 +0530

    Completed Day 1

---
+++ src/SecondLargestElement.java
@@ -0,0 +1,60 @@
+import java.io.PrintStream;
+import java.util.Arrays;
+
+public class SecondLargestElement {
+
+    private static int[] arr;
+
+    public static void main(String[] args) {
+
+        int[] arr = {12, 35, 1, 10, 34, 1};
+
+        int result = method3(arr);
+        System.out.println(result);
+    }
+
+    static int method1(int[] arr) {
+
+        Arrays.sort(arr);
+
+        for (int i = arr.length - 2; i >= 0; i--) {
+            if (arr[i] != arr[arr.length - 1]) return arr[i];
+        }
+
+        return -1;
+    }
+
+    static int method2(int[] arr) {
+
+        int largest = Integer.MIN_VALUE;
+
+        for (int j : arr) {
+            if (j > largest) largest = j;
+        }
+
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int j : arr) {
+            if (j > secondLargest && j != largest) secondLargest = j;
+        }
+
+        return secondLargest;
+    }
+
+    static int method3(int[] arr) {
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int j : arr) {
+            if (j > largest) {
+                secondLargest = largest;
+                largest = j;
+            } else if (j < largest && j > secondLargest) {
+                secondLargest = j;
+            }
+        }
+        return secondLargest;
+    }
+
+}
