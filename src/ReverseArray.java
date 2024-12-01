ae908854deb74d1be29945ed23014867156d8db1
Hari haran <hariharan15060@gmail.com>
2024-12-01 20:01:57 +0530

    Day 3

--- src/ReverseArray.java
+++ src/ReverseArray.java
@@ -4,9 +4,9 @@ public class ReverseArray {
     public static void main(String[] args){
         int[] arr = { 1, 4, 3, 2, 6, 5 };
 
-        method1(arr); //Creating a temp array
+//        method1(arr); //Creating a temp array
 //        method2(arr); //By using swap method
-//        method3(arr);
+        method3(arr); //Using for loop and iterating only half
 
         System.out.println(Arrays.toString(arr));
     }
@@ -25,11 +25,26 @@ public class ReverseArray {
     }
 
     static void method2(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
 
+        while (start <= end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
     }
 
     static void method3(int[] arr){
+        int n = arr.length;
 
+        for(int i = 0; i < n/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n -i -1];
+            arr[n -i -1] = temp;
+        }
     }
 
 }
