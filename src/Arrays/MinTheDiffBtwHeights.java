df5fbc652ac0d0e5cca5bf72cfc27d305821164b
Hari haran <hariharan15060@gmail.com>
2024-12-14 23:23:11 +0530

    Day 8

---
+++ src/Arrays/MinTheDiffBtwHeights.java
@@ -0,0 +1,30 @@
+package Arrays;
+
+import java.util.Arrays;
+
+public class MinTheDiffBtwHeights {
+    public static void main(String[] args){
+        int k = 6;
+        int[] arr = {12, 6, 4, 15, 17, 10};
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+        int res = arr[n - 1] - arr[0];
+
+        for(int i = 1; i < n; i++) {
+
+            if (arr[i] - k < 0) continue;
+
+            // Minimum height after modification
+            int minH = Math.min(arr[0] + k, arr[i] - k);
+
+            // Maximum height after modification
+            int maxH = Math.max(arr[i - 1] + k, arr[n - 1] - k);
+
+            // Store the minimum difference as result
+            res = Math.min(res, maxH - minH);
+        }
+        System.out.println(res);
+    }
+}
